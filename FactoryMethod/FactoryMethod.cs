7e1b3bae1d1393df6a8602adb6afff8d53ca83a5
andre.soares <andre.soares@zup.com.br>
2020-03-21 01:00:04 -0300

    Factory Method

--- FactoryMethod/FactoryMethod.cs
+++ FactoryMethod/FactoryMethod.cs
@@ -1,15 +1,27 @@
+using System;
+using System.Linq;
+using System.Reflection;
+
 namespace FactoryMethod
 {
     public class FactoryMethod
     {
         public ICharacter ChooseCharacter(string name)
         {
-            switch (name)
-            {
-                case "SubZero": return new SubZero();
-                case "Scorpion": return new Scorpion();
-                default: return default;
-            }
+           
+            var type = Assembly
+            .GetExecutingAssembly()
+            .GetTypes()
+            .Where(x => x.GetInterfaces().Any(y => y.Name == "ICharacter") &&  x.Name == name)
+            .FirstOrDefault();
+
+            if(type is null) return default;
+
+
+            return (ICharacter)Activator.CreateInstance(Type.GetType(type.FullName));
+
+           
+
         }
     }
 }
